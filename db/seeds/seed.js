bc804c108612ae632fe30d2a4cd5cbcad8dee6d5
Andrew <manloengchung@yahoo.co.uk>
2019-06-24 16:32:07 +0100

    connected formatted Article table

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -3,50 +3,36 @@ const { topicData, articleData, commentData, userData } = require('../index.js')
 const { formatDate, formatComments, makeRefObj } = require('../utils/utils');
 
 exports.seed = function(knex, Promise) {
-	return knex.migrate
-		.rollback()
-		.then(() => knex.migrate.latest())
-		.then(() => {
-			return knex('topics').insert(topicData).returning('*');
-		})
-		.then(() => {
-			knex('users').insert(userData).returning('*');
-		})
-		.then(() => {
-			console.log(userData);
-		});
-	// const topicsInsertions = knex('topics').insert(topicData);
-	// const usersInsertions = knex('users').insert(userData);
+	const topicsInsertions = knex('topics').insert(topicData);
+	const usersInsertions = knex('users').insert(userData);
 
-	return Promise.all([ topicsInsertions, usersInsertions ])
-		.then(() => {
-			const formattedArticle = formatDate(articleData);
-			// console.log(formattedArticle);
-			// return knex('articles').insert(formattedArticle).returning('*').then((result) => {
-			// 	console.log(result);
-			// });
-			/* 
-      
-      Your article data is currently in the incorrect format and will violate your SQL schema. 
-      
-      You will need to write and test the provided formatDate utility function to be able insert your article data.
+	return knex.migrate.rollback().then(() => knex.migrate.latest()).then(() => {
+		return Promise.all([ topicsInsertions, usersInsertions ])
+			.then(() => {
+				topicsInsertions.returning('*');
+			})
+			.then(() => {
+				usersInsertions.returning('*');
+			})
+			.then(() => {
+				const formattedArticle = formatDate(articleData);
+				return knex('articles').insert(formattedArticle).returning('*');
+			})
+			.then((articleRows) => {
+				// console.log(result);
+				console.log(articleRows);
+				/* 
 
-      Your comment insertions will depend on information from the seeded articles, so make sure to return the data after it's been seeded.
-      */
-			// map through article data, change created_at = (some number) to created_at = new Date(some number)
-		})
-		.then((articleRows) => {
-			/* 
+  Your comment data is currently in the incorrect format and will violate your SQL schema. 
 
-      Your comment data is currently in the incorrect format and will violate your SQL schema. 
+  Keys need renaming, values need changing, and most annoyingly, your comments currently only refer to the title of the article they belong to, not the id. 
+  
+  You will need to write and test the provided makeRefObj and formatComments utility functions to be able insert your comment data.
+  */
 
-      Keys need renaming, values need changing, and most annoyingly, your comments currently only refer to the title of the article they belong to, not the id. 
-      
-      You will need to write and test the provided makeRefObj and formatComments utility functions to be able insert your comment data.
-      */
-
-			const articleRef = makeRefObj(articleRows);
-			const formattedComments = formatComments(commentData, articleRef);
-			return knex('comments').insert(formattedComments);
-		});
+				const articleRef = makeRefObj(articleRows);
+				const formattedComments = formatComments(commentData, articleRef);
+				return knex('comments').insert(formattedComments);
+			});
+	});
 };
