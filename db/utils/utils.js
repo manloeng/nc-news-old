6b3dfca1b8a26700593cbebd02990636d73fd0bd
Andrew <manloengchung@yahoo.co.uk>
2019-06-24 21:17:50 +0100

    passing for all tests

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -17,30 +17,18 @@ exports.makeRefObj = (list) => {
 
 exports.formatComments = (comments, articleRef) => {
 	if (!Object.keys(comments[0]).length) return {};
+
 	comments.forEach((comment) => {
-		if (Object.keys(comment)[0] === 'created_at') {
-			comment.created_at = new Date(comment.created_at);
-		}
-		if (Object.keys(comment)[0] === 'belongs_to') {
-			comment.article_id = articleRef[comment.belongs_to];
-			delete comment.belongs_to;
-		}
+		const keys = Object.keys(comment);
+		keys.forEach((key) => {
+			if (key === 'created_at') {
+				comment.created_at = new Date(comment.created_at);
+			}
+			if (key === 'belongs_to') {
+				comment.article_id = articleRef[comment.belongs_to];
+				delete comment.belongs_to;
+			}
+		});
 	});
 	return comments;
 };
-
-/**
- * 		(datevalues = [
-			date.getDate(),
-			date.getMonth() + 1,
-			date.getFullYear(),
-			date.getHours() - 1,
-			date.getMinutes(),
-			date.getSeconds(),
-			date.getMilliseconds()
-		]);
-	console.log(datevalues[0]);
-	let formattedDate = `${datevalues[0]} ${datevalues[1]} ${datevalues[2]} ${datevalues[3]}:${datevalues[4]}:${datevalues[5]}:${datevalues[6]}`;
-	console.log(formattedDate);
-	return formattedDate;
- */
