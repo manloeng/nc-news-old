40efda76615ff3f7a8516193fadc76001acca643
Andrew <manloengchung@yahoo.co.uk>
2019-06-24 13:58:09 +0100

    passing the second test

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,7 +1,25 @@
 exports.formatDate = (list) => {
-	return null;
+	if (!list) return null;
+	let date = new Date(list[0]);
+	return date;
 };
 
 exports.makeRefObj = (list) => {};
 
 exports.formatComments = (comments, articleRef) => {};
+
+/**
+ * 		(datevalues = [
+			date.getDate(),
+			date.getMonth() + 1,
+			date.getFullYear(),
+			date.getHours() - 1,
+			date.getMinutes(),
+			date.getSeconds(),
+			date.getMilliseconds()
+		]);
+	console.log(datevalues[0]);
+	let formattedDate = `${datevalues[0]} ${datevalues[1]} ${datevalues[2]} ${datevalues[3]}:${datevalues[4]}:${datevalues[5]}:${datevalues[6]}`;
+	console.log(formattedDate);
+	return formattedDate;
+ */
