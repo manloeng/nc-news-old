4f61afd8803552820d8a5ee765b842834384e4ce
Andrew <manloengchung@yahoo.co.uk>
2019-06-24 11:00:29 +0100

    created index.js file and imported data

---
+++ db/test-data/index.js
@@ -0,0 +1,6 @@
+const articlesData = require('./articles');
+const commentsData = require('./comments');
+const topicsData = require('./topics');
+const usersData = require('./users');
+
+module.exports = { articlesData, commentsData, topicsData, usersData };
