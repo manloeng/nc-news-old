4d639a99409f97063c5c23dfa501403bdc143db6
Andrew <manloengchung@yahoo.co.uk>
2019-06-24 15:05:48 +0100

    amended the test with new expected results

--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -9,8 +9,13 @@ describe('formatDate', () => {
 	it('returns the converted date when passed a Unix timestamp of 0', () => {
 		const input = [ { created_at: 0 } ];
 		const result = formatDate(input);
-		expect(result).to.be.eql(new Date(0));
+		expect(result).to.be.eql([ { created_at: new Date(0) } ]);
 	});
+	// it('returns the converted date when passed with multiple Unix timestamp in an array', () => {
+	// 	const input = [ { created_at: 0 }, { created_at: 1561384201 }, { created_at: 1561303241 } ];
+	// 	const result = formatDate(input);
+	// 	expect(result).to.be.eql(new Date(0));
+	// });
 });
 
 describe('makeRefObj', () => {});
