9e0fee8d0e481aea295a3217b54789fc0870c3fa
Andrew <manloengchung@yahoo.co.uk>
2019-06-24 11:52:33 +0100

    Created second test to format the date

--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -6,6 +6,11 @@ describe('formatDate', () => {
 		const result = formatDate();
 		expect(result).to.be.null;
 	});
+	it('returns the converted date when passed a Unix timestamp of 0', () => {
+		const input = 0;
+		const result = formatDate(input);
+		expect(result).to.be.equal('January 1, 1970 at 00:00:00');
+	});
 });
 
 describe('makeRefObj', () => {});
