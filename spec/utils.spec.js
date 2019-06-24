7f45c0171a3ca91612c2e0f4af79000949bba0d8
Andrew <manloengchung@yahoo.co.uk>
2019-06-24 11:44:28 +0100

    added base test, for our format date func

--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -1,7 +1,12 @@
 const { expect } = require('chai');
 const { formatDate, makeRefObj, formatComments } = require('../db/utils/utils');
 
-describe('formatDate', () => {});
+describe('formatDate', () => {
+	it('returns null when passed without any integers', () => {
+		const result = formatDate();
+		expect(result).to.be.null;
+	});
+});
 
 describe('makeRefObj', () => {});
 
