aed9a7bc6e2047acbe405f9105ac68d5b423ddfd
Andrew <manloengchung@yahoo.co.uk>
2019-06-24 14:21:14 +0100

    created the articles migration file

---
+++ db/migrations/20190624141108_create_articles_table.js
@@ -0,0 +1,15 @@
+exports.up = function(knex, Promise) {
+	return knex.schema.createTable('articles', function(articlesTable) {
+		articlesTable.string('article_id').primary();
+		articlesTable.string('title').notNullable();
+		articlesTable.text('body');
+		articlesTable.integer('votes').defaultTo(0);
+		articlesTable.string('topic').references('topics.slug');
+		articlesTable.string('author').references('users_username');
+		articlesTable.timestamp('created_at');
+	});
+};
+
+exports.down = function(knex, Promise) {
+	return knex.schema.dropTable('articles');
+};
