68cf4c343a04ea0909aade5cfec23978d6d29aae
Andrew <manloengchung@yahoo.co.uk>
2019-06-24 11:55:36 +0100

    changed the input in the 2nd test, to an array

--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -7,7 +7,7 @@ describe('formatDate', () => {
 		expect(result).to.be.null;
 	});
 	it('returns the converted date when passed a Unix timestamp of 0', () => {
-		const input = 0;
+		const input = [ 0 ];
 		const result = formatDate(input);
 		expect(result).to.be.equal('January 1, 1970 at 00:00:00');
 	});
