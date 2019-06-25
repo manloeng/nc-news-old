90bdb28d8aedd72176fdfcfe2644c4639d7483c8
Andrew <manloengchung@yahoo.co.uk>
2019-06-25 08:22:08 +0100

    adjusted seed file, so data can be inserted

--- db/migrations/20190624141116_create_comments_table.js
+++ db/migrations/20190624141116_create_comments_table.js
@@ -1,11 +1,11 @@
 exports.up = function(knex, Promise) {
 	return knex.schema.createTable('comments', function(commentsTable) {
-		commentsTable.integer('comment_id').primary();
+		commentsTable.increments('comment_id').primary();
 		commentsTable.string('author').references('users_username');
 		commentsTable.integer('article_id').references('articles.article_id');
 		commentsTable.integer('votes').defaultTo(0);
 		commentsTable.timestamp('created_at');
-		commentsTable.string('body');
+		commentsTable.text('body');
 	});
 };
 
