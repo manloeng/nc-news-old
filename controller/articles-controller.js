3155394df10f68cc1adcd5893ad78c0d724faeff
Andrew <manloengchung@yahoo.co.uk>
2019-06-25 22:32:31 +0100

    connected comments to articles

--- controller/articles-controller.js
+++ controller/articles-controller.js
@@ -1,4 +1,5 @@
 const { fetchArticles, fetchArticlesById } = require('../model/articles-model.js');
+const { fetchCommentsByArticleId } = require('../model/comments.model.js');
 
 const sendArticles = () => {
 	// console.log('sendArticles here');
@@ -8,9 +9,13 @@ const sendArticles = () => {
 const sendArticlesById = (req, res, next) => {
 	// console.log('sendArticlesByID here');
 	const article_id = req.params;
-	fetchArticlesById(article_id)
-		.then((article) => {
-			res.status(200).send({ article });
+	fetchCommentsByArticleId(article_id)
+		.then((result) => {
+			const commentLength = result.length;
+			// console.log(commentLength);
+			fetchArticlesById(article_id, commentLength).then((article) => {
+				res.status(200).send({ article });
+			});
 		})
 		.catch(next);
 };
--- model/articles-model.js
+++ model/articles-model.js
@@ -2,14 +2,19 @@ const connection = require('../db/connection.js');
 
 const fetchArticles = () => {
 	console.log('fetchArticles here');
+	// return connection.first('*').from('articles').then((article) => {
+	// 	console.log(article);
+	// 	return article;
+	// });
 };
 
-const fetchArticlesById = (article) => {
+const fetchArticlesById = (article, length) => {
 	// console.log('fetchArticlesByID here');
-	console.log(article);
-	return connection.first('*').from('articles').where('article_id', article.article_id).then((article) => {
-		return article;
+	return connection.select('*').from('articles').where('article_id', article.article_id).then((formattedArticle) => {
+		formattedArticle[0].comment_count = length;
+		return formattedArticle[0];
 	});
 };
+//.where('article_id', article.article_id)
 
 module.exports = { fetchArticles, fetchArticlesById };
