a9abb4b052f2fbf548ab4a4b9af3befc8e4ddac4
Andrew <manloengchung@yahoo.co.uk>
2019-06-25 11:48:41 +0100

    connected models

--- controller/users-controller.js
+++ controller/users-controller.js
@@ -1,5 +1,8 @@
+const { fetchUsers } = require('../model/users-model.js');
+
 const sendUsers = () => {
 	console.log('sending users');
+	fetchUsers();
 };
 
 module.exports = { sendUsers };
---
+++ model/users-model.js
@@ -0,0 +1,5 @@
+const fetchUsers = () => {
+	console.log('fetching users');
+};
+
+module.exports = { fetchUsers };
