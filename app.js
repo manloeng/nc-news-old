aae5fa81ddc7648825f4a26d4af34975abb91ff2
Andrew <manloengchung@yahoo.co.uk>
2019-06-26 03:34:29 +0100

    added express.json()

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express('');
 const apiRouter = require('./routes/apiRouter.js');
 
+app.use(express.json());
 app.use('/api', apiRouter);
 
 app.all('/*', (req, res) => {
