1abb658753d45859d322284bfaf872a88cae0c6f
Andrew <manloengchung@yahoo.co.uk>
2019-06-26 12:27:43 +0100

    updated error handling

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use((err, req, res, next) => {
 		res.status(err.status).send({ msg: err.msg });
 	}
 	if (err.code === '22P02') {
-		res.status(400).send({ msg: 'Invalid article ID' });
+		res.status(400).send({ msg: err.message });
 	}
 });
 
