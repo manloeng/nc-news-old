96bffb7c5236ad315ade7dc5f70630f7a1d7d3e3
Andrew <manloengchung@yahoo.co.uk>
2019-06-26 11:28:45 +0100

    tests passing for the endpoint /:article_id

--- model/articles-model.js
+++ model/articles-model.js
@@ -7,8 +7,22 @@ const fetchArticlesById = (article_id) => {
 		.from('articles')
 		.join('comments', 'articles.article_id', 'comments.article_id')
 		.groupBy('articles.article_id')
-		.where('articles.article_id', article_id);
+		.where('articles.article_id', article_id)
+		.then((article) => {
+			if (!article) {
+				return Promise.reject({
+					status: 404,
+					msg: 'Article ID Not Found'
+				});
+			} else {
+				return article;
+			}
+		});
 };
+
+// SELECT articles.*, COUNT(comments.article_id) FROM articles
+// JOIN comments ON articles.article_id = comments.article_id
+// GROUP BY articles.article_id;
 
 const updateArticleVote = (article, voteCounter, commentLength) => {
 	return connection.first('*').from('articles').where('article_id', article.article_id).then((article) => {
