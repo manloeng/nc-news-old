3e49441f8dee5f1df957c380b2c004237df7c34c
Andrew <manloengchung@yahoo.co.uk>
2019-06-28 10:59:57 +0100

    removed comments

--- errors/check.js
+++ errors/check.js
@@ -1,13 +1,8 @@
 const connection = require('../db/connection.js');
 
 const checkIfExists = (id, table, column) => {
-	return connection.select('*').from(table).where(column, id).then((row) => {
-		console.log(row);
-		console.log(row.length);
-		if (row.length === 0) {
-			console.log('HERE Passing');
-			return true;
-		} else return false;
+	return connection.select('*').from(table).where(column, id).then((rows) => {
+		return rows.length !== 0;
 	});
 };
 
