604b00d5fbe78f2b442346a00c6eeddf6a53c3ab
Andrew <manloengchung@yahoo.co.uk>
2019-06-28 12:12:43 +0100

    adding endpoints

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -3,10 +3,20 @@ const topicsRouter = require('./topicsRouter.js');
 const userRouter = require('./userRouter.js');
 const articleRouter = require('./articleRouter.js');
 const commentsRouter = require('./commentsRouter.js');
+const { sendMethodNotAllowed } = require('../errors/errors.js');
+const json = require('../endpoints.json');
 
 apiRouter.use('/topics', topicsRouter);
 apiRouter.use('/users', userRouter);
 apiRouter.use('/articles', articleRouter);
 apiRouter.use('/comments', commentsRouter);
+
+apiRouter
+	.route('/')
+	.get((req, res) => {
+		console.log(json);
+		res.status(200).send(json);
+	})
+	.all(sendMethodNotAllowed);
 
 module.exports = apiRouter;
