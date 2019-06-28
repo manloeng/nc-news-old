a961e88f14f22abc8785e8d402f1a349a6b02a47
Andrew <manloengchung@yahoo.co.uk>
2019-06-28 15:08:07 +0100

    Console.log

--- db/index.js
+++ db/index.js
@@ -2,12 +2,12 @@ const ENV = process.env.NODE_ENV || 'development';
 
 const devData = require('./development-data');
 const testData = require('./test-data');
-
 // console.log(testData);
 
 const data = {
 	development: devData,
 	test: testData,
+	production: devData
 };
 
 module.exports = data[ENV];
