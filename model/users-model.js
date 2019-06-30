327f3219abf69e1a438bf44978ebbb094876ff98
Andrew <manloengchung@yahoo.co.uk>
2019-06-30 14:28:03 +0100

    removed comment

--- model/users-model.js
+++ model/users-model.js
@@ -2,7 +2,6 @@ const connection = require('../db/connection.js');
 
 const fetchUsersById = (userId) => {
 	return connection.first('*').from('users').where('username', userId.username).then((user) => {
-		console.log(user);
 		if (!user) {
 			return Promise.reject({
 				status: 404,
