a5e84949a1f176b280d7dbc22df266b405806a05
Andrew <manloengchung@yahoo.co.uk>
2019-06-30 18:51:15 +0100

    exported errors to err folder & add status 500 err

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express('');
 const apiRouter = require('./routes/apiRouter.js');
+const { errStatus, sqlErrors, internalServerErrors } = require('./errors/errors.js');
 
 app.use(express.json());
 app.use('/api', apiRouter);
@@ -9,31 +10,8 @@ app.all('/*', (req, res) => {
 	res.status(404).send({ msg: 'Page Not Found' });
 });
 
-app.use((err, req, res, next) => {
-	if (err.status) {
-		res.status(err.status).send({ msg: err.msg });
-	}
-
-	const sqlErrorCode = {
-		'22P02': '22P02',
-		'42703': '42703'
-	};
-
-	if (err.code in sqlErrorCode) {
-		res.status(400).send({ msg: err.message });
-	} else {
-		next(err);
-	}
-	// const sqlErrorCode = [ '22P02', '42703' ];
-	// if (sqlErrorCode.includes(err.code)) {
-	// 	res.status(400).send({ msg: err.message });
-	// } else {
-	// 	next(err);
-	// }
-});
-
-app.use((err, req, res, next) => {
-	res.status(500).send({ msg: 'Internal Sever Error' });
-});
+app.use(errStatus);
+app.use(sqlErrors);
+app.use(internalServerErrors);
 
 module.exports = app;
--- errors/errors.js
+++ errors/errors.js
@@ -2,4 +2,34 @@ const sendMethodNotAllowed = (req, res) => {
 	res.status(405).send({ msg: 'Method Not Allowed' });
 };
 
-module.exports = { sendMethodNotAllowed };
+const errStatus = (err, req, res, next) => {
+	if (err.status) {
+		res.status(err.status).send({ msg: err.msg });
+	}
+	next(err);
+};
+
+const sqlErrors = (err, req, res, next) => {
+	const sqlErrorCode = {
+		'22P02': '22P02',
+		'42703': '42703'
+	};
+
+	if (err.code in sqlErrorCode) {
+		res.status(400).send({ msg: err.message });
+	} else {
+		next(err);
+	}
+	// const sqlErrorCode = [ '22P02', '42703' ];
+	// if (sqlErrorCode.includes(err.code)) {
+	// 	res.status(400).send({ msg: err.message });
+	// } else {
+	// 	next(err);
+	// }
+};
+
+const internalServerErrors = (err, req, res, next) => {
+	res.status(500).send({ msg: 'Internal Sever Error' });
+};
+
+module.exports = { sendMethodNotAllowed, errStatus, sqlErrors, internalServerErrors };
